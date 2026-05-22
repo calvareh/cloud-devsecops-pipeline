2e1523bd7fec74176dcf9cd417d4cca06453ab0f
Cristian <cristian.alvarezhd@gmail.com>
2026-05-21 22:13:38 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, render_template_string
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
-    name = request.args.get("name", "world")
-    return render_template_string("Hello {{ name }}", name=name)
+    return "Hello world"
