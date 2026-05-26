0aea010cc4719ded1f067371b49f03eebd802b3e
Cristian <cristian.alvarezhd@gmail.com>
2026-05-26 13:22:06 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "DevSecOps demo app is running"
+
 @app.route("/ping")
 def ping():
-    return "Hello world"
+    return "pong"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
